762421c26eb93ba77327b6f67ea9a3ed77b6ea03
Student User <student@workstation.lab.example.com>
2024-03-16 21:59:53 -0400

    rectifying syntax

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,5 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "I wil crack both exams, Ex467 as well as Ex188. I am damn sure! "\n";
+print "I wil crack both exams, Ex467 as well as Ex188. I am damn sure!" "\n";
+
 ?>
